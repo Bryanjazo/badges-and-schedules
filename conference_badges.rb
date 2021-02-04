7b7d86f0f6437d719f0129473ad9ee5ca359a7ec
Bryanjazo <bryanjazo88@gmail.com>
2021-02-04 14:00:07 -0800

    commit tree 7a968f089adcd8ae3b4ae37cf6ad3f6e15ff0ca2

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+require "pry"
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    new_array =[]
+    attendees.each do |name|
+        new_array.push("Hello, my name is #{name}.")
+    end
+    return new_array
+end
+
+def assign_rooms(attendees) 
+    new_array = []
+    attendees.each_with_index do |name, index|
+        new_array.push("Hello, #{name}! You'll be assigned to room #{index+1}!")
+    end
+    return new_array
+end
+
+def printer(attendees)
+    new_array = []
+    attendees.each_with_index do |name, index|
+     puts "Hello, my name is #{name}."
+     puts "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    end
+end
